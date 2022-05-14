b0414ca45ee9a00bba50bfc4edac253671790790
Vinnie Moscaritolo <vinnie@vmeng.com>
2022-05-14 11:06:47 -0700

    no message

--- src/main.cpp
+++ src/main.cpp
@@ -73,11 +73,11 @@ int main(int argc, const char * argv[]) {
 					
 					printf("Moved %s ", cw? "CW": "CCW");
 					
-					uint8_t red = 128;
+					uint8_t red = 0xff;
 					uint8_t green = 128;
-					uint8_t blue = 128;
+					uint8_t blue = 0xff;
 					
-					green +=  (cw? 1:-1);
+					green +=  (cw? 8:-8);
 					
 					duppa.setColor(red, green, blue);
 
